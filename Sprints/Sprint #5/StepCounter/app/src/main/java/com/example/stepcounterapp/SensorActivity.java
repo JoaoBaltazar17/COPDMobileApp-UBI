942306453d8e70dc13b0f189586940776fe39966
João Baltazar <joaobaltazar55@gmail.com>
2023-04-01 12:32:55 +0100

    .

--- Sprints/Sprint #5/StepCounter/app/src/main/java/com/example/stepcounterapp/SensorActivity.java
+++ Sprints/Sprint #5/StepCounter/app/src/main/java/com/example/stepcounterapp/SensorActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -18,17 +19,21 @@ public class SensorActivity extends Activity implements SensorEventListener {
     private SensorManager oSM;
     private Sensor oL;
 
-    private boolean bMessage = false;
+    private static final String TAG = "StepDetector";
+    private static final int STEP_THRESHOLD = 10;
 
-    private float previousXX = 0;
-    private float previousYY = 0;
-    private float previousZZ = 0;
+    private int stepCount = 0;
+    private boolean isStepDetected = false;
+
+
+    TextView txtVPassos;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
 
+        txtVPassos = findViewById(R.id.stepCountTextView);
         oSM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         oL = oSM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
@@ -50,26 +55,22 @@ public class SensorActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
-
-        float aXX = sensorEvent.values[0];
-        float aYY = sensorEvent.values[1];
-        float aZZ = sensorEvent.values[2];
-
-        float deltaXX = aXX - previousXX;
-        previousXX = aXX;
-
-        float deltaYY = aYY - previousYY;
-        previousYY = aYY;
-
-        float deltaZZ = aZZ - previousZZ;
-        previousZZ = aZZ;
-
-        if(deltaXX != 0 | deltaYY != 0 | deltaZZ != 0) {
-            if(!bMessage) {
-                System.out.println("X: " + aXX + "\n" + "Y: " + aYY + "\n" + "Z: " + aZZ);
+            float acceleration = (float) Math.sqrt(x * x + y * y + z * z) - SensorManager.GRAVITY_EARTH;
+            if (acceleration > STEP_THRESHOLD && !isStepDetected) {
+                isStepDetected = true;
+                stepCount++;
             }
+            if (acceleration < STEP_THRESHOLD && isStepDetected) {
+                isStepDetected = false;
+            }
+            txtVPassos.setText("Passos: " + stepCount);
+
         }
     }
 
