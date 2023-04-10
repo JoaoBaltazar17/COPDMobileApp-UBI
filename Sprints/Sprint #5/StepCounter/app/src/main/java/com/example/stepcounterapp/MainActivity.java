db86d1ce97f9331f0e533a60729bef26bb6d1078
João Baltazar <joaobaltazar55@gmail.com>
2023-04-10 21:41:44 +0100

    .

--- Sprints/Sprint #5/StepCounter/app/src/main/java/com/example/stepcounterapp/MainActivity.java
+++ Sprints/Sprint #5/StepCounter/app/src/main/java/com/example/stepcounterapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView stepCountTextView;
 
+    private static final int SEQUENCE_SIZE = 10; // Number of sensor events to store for analysis
+    private static final int STEP_THRESHOLD = 10;
+    private float[] sensorData = new float[SEQUENCE_SIZE * 3]; // Store accelerometer sensor values for X, Y, and Z axes
+    private int sensorDataIndex = 0;
+    private int stepCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,20 +33,40 @@ public class MainActivity extends AppCompatActivity {
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         stepCountTextView = findViewById(R.id.stepCountTextView);
+
     }
 
     private SensorEventListener sensorEventListener = new SensorEventListener() {
-        private static final int STEP_THRESHOLD = 10;
-        private float previousY = 0;
-        private int stepCount = 0;
         @Override
         public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
             float y = event.values[1];
-            float deltaY = y - previousY;
-            previousY = y;
-            if (Math.abs(deltaY) > STEP_THRESHOLD) {
-                stepCount++;
-                updateStepCount(stepCount);
+            float z = event.values[2];
+
+            // Store the sensor values in the sensorData array
+            sensorData[sensorDataIndex++] = x;
+            sensorData[sensorDataIndex++] = y;
+            sensorData[sensorDataIndex++] = z;
+
+            // Check if enough sensor events have been collected for analysis
+            if (sensorDataIndex == SEQUENCE_SIZE * 3) {
+                // Analyze the difference between two consecutive sequences to identify a step
+                float deltaX = 0;
+                float deltaY = 0;
+                float deltaZ = 0;
+                for (int i = 0; i < SEQUENCE_SIZE - 1; i++) {
+                    deltaX += sensorData[i * 3 + 3] - sensorData[i * 3];
+                    deltaY += sensorData[i * 3 + 3 + 1] - sensorData[i * 3 + 1];
+                    deltaZ += sensorData[i * 3 + 3 + 2] - sensorData[i * 3 + 2];
+                }
+
+                if (Math.abs(deltaX) > STEP_THRESHOLD || Math.abs(deltaY) > STEP_THRESHOLD || Math.abs(deltaZ) > STEP_THRESHOLD) {
+                    stepCount++;
+                    updateStepCount(stepCount);
+                }
+
+                // Reset the sensorDataIndex for the next sequence
+                sensorDataIndex = 0;
             }
         }
 
