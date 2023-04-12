f7b03d72cc21e63df0ac22b4bedae489c49f817b
João Baltazar <joaobaltazar55@gmail.com>
2023-04-12 21:52:44 +0100

    .

--- COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/InitialPage.java
+++ COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/InitialPage.java
@@ -6,20 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class InitialPage extends AppCompatActivity {
 
-    Button btnSignUpPage;
+    TextView txtViewGoRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_page);
 
-        btnSignUpPage = findViewById(R.id.btnSignUp);
+        txtViewGoRegister = findViewById(R.id.txtViewRegisterClick);
     }
 
-    public void goToSignUpPage(View view) {
+    public void onRegisterLinkClicked(View view) {
         Intent intent = new Intent(this, RegisterPage.class);
         startActivity(intent);
     }
