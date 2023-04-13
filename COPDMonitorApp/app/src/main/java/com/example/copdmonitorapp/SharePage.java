aa34c4bf110f2ee80921086e3ddfe7b3c3210279
João Baltazar <joaobaltazar55@gmail.com>
2023-04-13 13:08:20 +0100

    .

--- COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/SharePage.java
+++ COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/SharePage.java
@@ -1,14 +1,110 @@
 package com.example.copdmonitorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SharePage extends AppCompatActivity {
+
+
+    // Navigation Drawer Attributes
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, settings, share, about, logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.share_page);
+        setContentView(R.layout.settings_page);
+
+
+        // Navigation Drawer Finders
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        about = findViewById(R.id.about);
+        logout = findViewById(R.id.logout);
+        settings = findViewById(R.id.settings);
+        share = findViewById(R.id.share);
+
+
+        // Menu Navigation and Components Listener's
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawer(drawerLayout);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(SharePage.this, HomePage.class);
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(SharePage.this, SettingsPage.class);
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(SharePage.this, AboutPage.class);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SharePage.this, "LogOut", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    // Navigation Drawer Methods
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity) {
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 }
