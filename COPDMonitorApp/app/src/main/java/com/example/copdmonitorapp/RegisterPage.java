79eb004432b4bd8f207576f585c88cd3bde76e51
João Baltazar <joaobaltazar55@gmail.com>
2023-04-13 06:27:25 +0100

    .

--- COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/RegisterPage.java
+++ COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/RegisterPage.java
@@ -1,13 +1,24 @@
 package com.example.copdmonitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterPage extends AppCompatActivity {
+
+    TextView TextVClickableGoLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+        TextVClickableGoLogin = findViewById(R.id.txtViewGoLogin);
+    }
+
+    public void onLoginLinkClicked(View view){
+        Intent intent = new Intent(this, InitialPage.class);
+        startActivity(intent);
     }
 }
