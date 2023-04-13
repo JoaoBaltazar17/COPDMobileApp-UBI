a4cd17d31ece7d9ae007058602520ff8cf5a87fc
João Baltazar <joaobaltazar55@gmail.com>
2023-04-13 07:34:39 +0100

    .

---
+++ COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/HomePage.java
@@ -0,0 +1,23 @@
+package com.example.copdmonitorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+
+    }
+
+
+}
