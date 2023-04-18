0b5200bb58c00dd53bbdd1ce5217dc2bbc891fca
João Baltazar <joaobaltazar55@gmail.com>
2023-04-18 22:44:46 +0100

    .

--- COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/ChatLivePage.java
+++ COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/ChatLivePage.java
@@ -27,7 +27,7 @@ public class ChatLivePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_page);
+        setContentView(R.layout.chatlive_page);
 
 
         // Navigation Drawer Finders
