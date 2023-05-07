b217ba485c6952392cc89a4a5314334370089c87
João Baltazar <joaobaltazar55@gmail.com>
2023-05-07 11:47:11 +0100

    .

--- COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/SettingsPage.java
+++ COPDMonitorApp/app/src/main/java/com/example/copdmonitorapp/SettingsPage.java
@@ -5,11 +5,14 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +20,13 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SettingsPage extends AppCompatActivity {
 
@@ -27,10 +37,37 @@ public class SettingsPage extends AppCompatActivity {
     LinearLayout home, settings, share, about, logout;
 
 
+    // TextViews
+    private TextView nameLogged;
+    private TextView emailLogged;
+    private TextView date_birthLogged;
+    private TextView heightLogged;
+    private TextView weightLogged;
+    private TextView copd_sevLogged;
+
+
+    // Variables
+
+    String email_shared;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
+
+        // SharedPreferences
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        email_shared = sharedPreferences.getString("email", "");
+
+        // TextViews's
+        nameLogged = findViewById(R.id.txtViewUsernameLogged);
+        emailLogged = findViewById(R.id.txtViewEmailLogged);
+        date_birthLogged = findViewById(R.id.txtViewDatelogged);
+        heightLogged = findViewById(R.id.txtViewHeightUserlogged);
+        weightLogged = findViewById(R.id.txtViewWeightUserlogged);
+        copd_sevLogged = findViewById(R.id.txtViewCOPDSeverityValue);
 
 
         // Navigation Drawer Finders
@@ -90,6 +127,8 @@ public class SettingsPage extends AppCompatActivity {
         });
 
 
+        new ProfileInformationTask().execute();
+
     }
 
     // Navigation Drawer Methods
@@ -121,5 +160,77 @@ public class SettingsPage extends AppCompatActivity {
 
     public void goToChangeUsername(View view) {
         redirectActivity(SettingsPage.this, SettingsUsernamePage.class);
+    }
+
+
+
+    private class ProfileInformationTask extends AsyncTask<String, Void, Boolean> {
+        private Exception exception;
+
+        private ProgressDialog progressDialog;
+
+
+        String name;
+        String email;
+        String date;
+        String copd_sev;
+        String height;
+        String weight;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(SettingsPage.this);
+            progressDialog.setMessage("Processing, please wait...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            String svurl = "jdbc:postgresql://copd-db-instance.cr6kvihylkhm.eu-north-1.rds.amazonaws.com:5432/copd_db";
+            String svusername = "postgres";
+            String svpassword = "copdproject";
+
+
+
+
+            try (Connection conn = DriverManager.getConnection(svurl, svusername, svpassword)) {
+
+                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Patient WHERE email = ?");
+                pstmt.setString(1, email_shared);
+                ResultSet rs = pstmt.executeQuery();
+
+                if (rs.next()) {
+                    name = rs.getString("name");
+                    date = rs.getString("date_birth mm/dd/aaaa");
+                    copd_sev = rs.getString("copd_severity");
+                    height = rs.getString("height (cm)");
+                    weight = rs.getString("weight (kg)");
+                    email = email_shared;
+                }
+
+                return false;
+            } catch (Exception e) {
+                Log.e("MyApp", "Error executing query", e);
+                exception = e;
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            progressDialog.dismiss();
+            nameLogged.setText(name);
+            emailLogged.setText(email);
+            date_birthLogged.setText(date);
+            heightLogged.setText(height);
+            weightLogged.setText(weight);
+            copd_sevLogged.setText(copd_sev);
+
+        }
+
     }
 }
